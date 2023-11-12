b3e6784b42649ec33b7eddb8468f207eb91b4a2c
Rahul Mondal <rahulmondar@gmail.com>
2023-11-12 09:40:58 +0530

    feat: minor change

--- src/app/api/issues/route.ts
+++ src/app/api/issues/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     try {
-        const body = await req.json();
+        const body: TCreateIssueInput = await req.json();
         const validate = CreateIssueSchema.safeParse(body);
         if (!validate.success) {
             return NextResponse.json(
